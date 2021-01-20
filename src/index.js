01ffb0aeb241fbc283ea261e3f44da1713c3776b
DraeRamsey <andrea.jos.ramsey@gmail.com>
2021-01-20 12:56:08 -0800

    create and style sections

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 // npx webpack
 // npx webpack --watch
 console.log("boops");
+
+var hamburger = document.getElementById('hamburger');
+
+hamburger.addEventListener('click', function(){
+  this.
+})
+
+
+// var hamburger = document.getElementById('hamburger')
+// undefined
+// hamburger.addEventListener('click', function(){})
+// undefined
+// hamburger.addEventListener('click', function(){ hamburger.classList.toggle('active')})
